637a3f33e4fcb613eb8445d6578ddbcd48c542f1
tangxin <tangxin@tangxin-mbp.local>
2019-01-14 17:50:42 +0800

    init3: 使用 docker 服务进行编译, 修改错误 dockerfile context 错误

--- autobuild.sh
+++ autobuild.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 #
 
-REPO=${REPO}
+[ -f  env.private ] && source env.private
+
 IMAGE=oracle-jdk
 TAG=11.0.1-alpine3.8
 
 USER=${HUB_MAIL}
 PASSWORD=${HUB_PASSWORD}
 
-docker login -u ${HUB_USER} -p ${HUB_PASSWD}
+# docker login -u ${HUB_USER} -p ${HUB_PASSWD}
 
-docker build -t ${REPO}/${IMAGE}:${TAG}
+docker build -t ${REPO}/${IMAGE}:${TAG} alpine-jdk
 docker push ${REPO}/${IMAGE}:${TAG}
 
