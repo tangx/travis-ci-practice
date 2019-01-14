8a4696f09ff77ebcd3836febd780fc0c4a9d0daa
tangxin <tangxin@tangxin-mbp.local>
2019-01-14 23:12:21 +0800

    测试 shell function

--- autobuild.sh
+++ autobuild.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 #
 
-[ -f  env.private ] && source env.private
+function build(){
+    echo "autobuild build"
+}
 
-IMAGE=oracle-jdk
-TAG=11.0.1-alpine3.8
+function pull(){
+    echo "autobuild pull"
+}
 
-USER=${HUB_MAIL}
-PASSWORD=${HUB_PASSWD}
-
-docker login -u ${HUB_USER} -p ${HUB_PASSWD}
-
-docker build -t ${REPO}/${IMAGE}:${TAG} alpine-jdk
-docker push ${REPO}/${IMAGE}:${TAG}
-
+case $1 in
+build) build ;;
+pull) pull ;;
+esac
