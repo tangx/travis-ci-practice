562dd074617e6c5c9921940cdf0af7e0f8d2abdf
tangxin <tangxin@tangxin-mbp.local>
2019-01-14 17:54:07 +0800

    init3: 使用 docker 服务进行编译, 修改错误 autobuild 变量错误

--- autobuild.sh
+++ autobuild.sh
@@ -9,7 +9,7 @@ TAG=11.0.1-alpine3.8
 USER=${HUB_MAIL}
 PASSWORD=${HUB_PASSWD}
 
-# docker login -u ${HUB_USER} -p ${HUB_PASSWD}
+docker login -u ${HUB_USER} -p ${HUB_PASSWD}
 
 docker build -t ${REPO}/${IMAGE}:${TAG} alpine-jdk
 docker push ${REPO}/${IMAGE}:${TAG}
